80713630ded70a3b9fadcba8f62e347f1adc53d0
Aydan Pirani <aydanpirani@gmail.com>
2021-07-23 18:23:15 +0000

    Added read script

--- public/script.js
+++ public/script.js
@@ -1,19 +1,50 @@
-const submitMessage = () => {
-    // const passcodeInput = document.querySelector('#tweet').value;
-    // const messageInput = document.querySelector('#message').value;
+const getPosts = () => {
+    console.log("Getting posts!");
+}
+
+const submitPost = () => {
+    // const usernameRef = document.querySelector("#username").value;
+    // const messageRef = document.querySelector("message").value;
     
+    // const username = usernameRef.value;
+    // const message = messageRef.value;
+
+    const username = "testUser";
+    const message = "testMessage#testHashtag";
+    const date = new Date();
+
+    const split = message.split("#");
+    const hashtags = split.slice(1, split.length).map(e => e.split(" ")[0]);
+
+    // usernameRef.value = "";
+    // messageRef.value = "";
+
     firebase.database().ref().push({
-        passcode: 'Hello',
-        message: 'hi'
+        username: username,
+        message: message, 
+        hashtags: hashtags, 
+        date: date
     });
-};
+    console.log("Submitted post!");
+}
 
-const getMessages = () => {
-const messagesRef = firebase.database().ref();
-messagesRef.on('value', (snapshot) => {
-    const data = snapshot.val();
-    console.log(data);
-});
 
-submitMessage();
-getMessages()
+// const submitMessage = () => {
+//     // const passcodeInput = document.querySelector('#tweet').value;
+//     // const messageInput = document.querySelector('#message').value;
+    
+//     firebase.database().ref().push({
+//         passcode: 'Hello',
+//         message: 'hi'
+//     });
+// };
+
+// const getMessages = () => {
+// const messagesRef = firebase.database().ref();
+// messagesRef.on('value', (snapshot) => {
+//     const data = snapshot.val();
+//     console.log(data);
+// });
+
+submitPost();
+// getMessages();
