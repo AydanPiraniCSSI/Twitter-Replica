86d307a377d97b54de716c588eb836122205c75b
Ara Oladipo <Oladipoeyiara@gmail.com>
2021-07-23 18:08:22 +0000

    Fixed database issue

--- public/script.js
+++ public/script.js
@@ -1,3 +1,13 @@
+const submitMessage = () => {
+    // const passcodeInput = document.querySelector('#tweet').value;
+    // const messageInput = document.querySelector('#message').value;
+    
+    firebase.database().ref().push({
+        passcode: 'Hello',
+        message: 'hi'
+    });
+};
+
 const getMessages = () => {
 const messagesRef = firebase.database().ref();
 messagesRef.on('value', (snapshot) => {
@@ -6,4 +16,5 @@ messagesRef.on('value', (snapshot) => {
 });
 }
 
+submitMessage();
 getMessages()
