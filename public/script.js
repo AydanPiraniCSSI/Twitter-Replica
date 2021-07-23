1f4f78da9a4ad16bc3af2d7902ec5b1a353ad925
Aydan Pirani <aydanpirani@gmail.com>
2021-07-23 17:53:53 +0000

    New firebase

--- public/script.js
+++ public/script.js
@@ -1,9 +1,7 @@
-const getMessages = () => {
 const messagesRef = firebase.database().ref();
 messagesRef.on('value', (snapshot) => {
     const data = snapshot.val();
     console.log(data);
 });
-}
 
 getMessages()
